9b8bd8231a38d85b3ddca122a3610443ec1733ee
Danilo Pezo <dpezomendoza@gmail.com>
2025-02-15 17:29:30 -0300

    Criada a model PessoaFisica

--- back-end/src/main/java/com/agenda/contatos/AgendaApplication.java
+++ back-end/src/main/java/com/agenda/contatos/AgendaApplication.java
@@ -1,4 +1,4 @@
-package com.desafio.agenda;
+package com.agenda.contatos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back-end/src/test/java/com/agenda/contatos/AgendaApplicationTests.java
+++ back-end/src/test/java/com/agenda/contatos/AgendaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.desafio.agenda;
+package com.agenda.contatos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
